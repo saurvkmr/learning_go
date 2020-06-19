2a4e6e093a7ec0f1b2fd8a15b926b0aa0c4918ec
saurvkmr <contact.xenon@gmail.com>
2020-06-19 21:28:44 +0530

    values

---
+++ basic/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName = "Saurav "
+	var secondName = "Kumar"
+	fmt.Println(firstName + secondName)
+	//fmt.Println(true + false)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(4.0 / 2.0)
+	fmt.Println(4 % 2)
+}
