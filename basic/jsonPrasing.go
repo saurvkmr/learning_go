b90e94279e8dabc360144a4a70ead94b2040e64c
saurvkmr <contact.xenon@gmail.com>
2020-06-28 23:26:16 +0530

    json parsing

--- basic/jsonPrasing.go
+++ basic/jsonPrasing.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/tidwall/gjson"
 )
 
+// https://github.com/public-apis/public-apis
 func main() {
-	response, err := http.Get("https://api.github.com/users/octocat/orgs")
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
 	if err == nil {
-		fmt.Println(response.Body)
+		responseStr, err := ioutil.ReadAll(response.Body)
+		if err == nil {
+			value := gjson.Get(string(responseStr), "descriptions")
+			//fmt.Println(reflect.TypeOf(value))
+			//fmt.Println(reflect.ValueOf(value))
+			fmt.Println(value)
+			//fmt.Println(string(responseStr))
+		}
 	}
 }
